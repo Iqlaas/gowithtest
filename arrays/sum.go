b8abf5f7a0dfa23bb6640241eb4c1340666e09a1
Iqlaas <kamaliqlaas@gmail.com>
2018-08-13 14:05:09 +0800

    3. Arrays

--- arrays/sum.go
+++ arrays/sum.go
@@ -1,5 +1,8 @@
 package main
 
+func main() {}
+
+//package main
 // Sum using for
 // func Sum(numbers [5]int) int {
 // 	sum := 0
@@ -16,4 +19,30 @@ func Sum(numbers []int) int {
 		sum += number
 	}
 	return sum
+}
+
+// SumAll function
+func SumAll(numbersToSum ...[]int) []int {
+	// lengthOfNumbers := len(numbersToSum)
+	// sums := make([]int, lengthOfNumbers)
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+	return sums
+}
+
+// SumAllTails function
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+	return sums
 }
--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -1,16 +1,16 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 
 	// t.Run("collection of 5 numbers", func(t *testing.T) {
-
 	// 	numbers := []int{1, 2, 3, 4, 5}
-
 	// 	got := Sum(numbers)
 	// 	want := 15
-
 	// 	if got != want {
 	// 		t.Errorf("got '%d' but want '%d', '%v'", got, want, numbers)
 	// 	}
@@ -28,4 +28,34 @@ func TestSum(t *testing.T) {
 		}
 	})
 
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+	// want := "kamal"
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got '%v' but '%v' given", got, want)
+	}
+}
+
+func TestSumAllTails(t *testing.T) {
+
+	checkSums := func(t *testing.T, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got '%v' want '%v'", got, want)
+		}
+	}
+	t.Run("make the sums of some slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+		checkSums(t, got, want)
+	})
+
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+		checkSums(t, got, want)
+	})
 }
