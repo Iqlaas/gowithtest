23a5b74d4920de6bdb049ef7f31e651b4f004fc4
Iqlaas <kamaliqlaas@gmail.com>
2018-08-21 16:49:41 +0800

    Up until Further refactoring

--- struct/perimeter.go
+++ struct/perimeter.go
@@ -1,8 +1,45 @@
 package main
 
+import (
+	"math"
+)
+
 func main() {}
 
 // Perimeter function
-func Perimeter(width, height float64) float64 {
-	return 0
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.width + rectangle.height)
+}
+
+// Area function
+// func Area(rectangle Rectangle) float64 {
+// 	return rectangle.width * rectangle.height
+// }
+
+// Rectangle type struct
+type Rectangle struct {
+	width  float64
+	height float64
+	// in tutorial, its capital. but it works with small.
+	// afaik on time of writing.
+}
+
+// Area method for rectangle
+func (r Rectangle) Area() float64 {
+	return r.width * r.height
+}
+
+// Circle type struct
+type Circle struct {
+	Radius float64
+}
+
+// Area method for circle
+func (c Circle) Area() float64 {
+	return c.Radius * c.Radius * math.Pi
+}
+
+// Shape interface
+type Shape interface {
+	Area() float64
 }
--- struct/perimeter_test.go
+++ struct/perimeter_test.go
@@ -3,10 +3,38 @@ package main
 import "testing"
 
 func TestPerimeter(t *testing.T) {
-	got := Perimeter(10.0, 10.0)
+	rectangle := Rectangle{10.0, 10.0}
+	got := Perimeter(rectangle)
 	want := 40.0
 
 	if got != want {
 		t.Errorf("got %.2f want %.2f", got, want)
 	}
+}
+
+func TestArea(t *testing.T) {
+
+	checkArea := func(t *testing.T, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	}
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{10.0, 10.0}
+		checkArea(t, rectangle, 100.0)
+		// got := rectangle.Area()
+		// want := 100.0
+
+		// if got != want {
+		// 	t.Errorf("got %.2f want %.2f", got, want)
+		// }
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10}
+		checkArea(t, circle, 314.1592653589793)
+	})
+
 }
