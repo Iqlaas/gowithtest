04480d5943ee779b9ab4d0752df5256de5776348
Iqlaas <kamaliqlaas@gmail.com>
2018-08-27 17:21:43 +0800

    Up until refactor

---
+++ pointers_errors/wallet.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Wallet struct {
+	balance int
+}
+
+func (w *Wallet) Deposit(amount int) {
+	fmt.Printf("address of balance in Deposit is %v \n", &w.balance)
+	w.balance += amount
+}
+
+func (w *Wallet) Balance() int {
+	return w.balance
+}
