3f71334d2f2949d4ac2224c5506c804209afcb61
Iqlaas <kamaliqlaas@gmail.com>
2018-08-28 16:22:11 +0800

    Done with pointers and errors.

--- pointers_errors/wallet.go
+++ pointers_errors/wallet.go
@@ -1,18 +1,34 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 )
 
-type Wallet struct {
-	balance int
+type Bitcoin int
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
 }
 
-func (w *Wallet) Deposit(amount int) {
-	fmt.Printf("address of balance in Deposit is %v \n", &w.balance)
+type Wallet struct {
+	balance Bitcoin
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
 }
 
-func (w *Wallet) Balance() int {
+var ErrInsufficientFunds = errors.New("cannot withdraw, insufficient funds")
+
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if amount > w.balance {
+		return ErrInsufficientFunds
+	}
+	w.balance -= amount
+	return nil
+}
+
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
 }
--- pointers_errors/wallet_test.go
+++ pointers_errors/wallet_test.go
@@ -1,20 +1,53 @@
 package main
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
-	wallet.Deposit(10)
 
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+		assertBalance(t, wallet, Bitcoin(10))
+	})
+
+	t.Run("Withdraw with funds", func(t *testing.T) {
+		wallet := Wallet{Bitcoin(20)}
+		err := wallet.Withdraw(Bitcoin(10))
+		assertBalance(t, wallet, Bitcoin(10))
+		assertNoError(t, err)
+	})
+
+	t.Run("Withdraw insufficient funds", func(t *testing.T) {
+		wallet := Wallet{Bitcoin(20)}
+		err := wallet.Withdraw(Bitcoin(30))
+		assertBalance(t, wallet, Bitcoin(20))
+		assertError(t, err, ErrInsufficientFunds)
+	})
+}
+
+func assertBalance(t *testing.T, wallet Wallet, want Bitcoin) {
+	t.Helper()
 	got := wallet.Balance()
-
-	fmt.Printf("address of balance in test is %v \n", &wallet.balance)
-	want := 10
-
 	if got != want {
-		t.Errorf("got %d want %d", got, want)
+		t.Errorf("got %s want %s", got, want)
+	}
+}
+
+func assertError(t *testing.T, got error, want error) {
+	t.Helper()
+	if got == nil {
+		t.Fatal("did not get an error but wanted one")
+	}
+	if got != want {
+		t.Errorf("got '%s', want '%s'", got, want)
+	}
+}
+
+func assertNoError(t *testing.T, got error) {
+	t.Helper()
+	if got != nil {
+		t.Fatal("got an error but did not want one")
 	}
 }
