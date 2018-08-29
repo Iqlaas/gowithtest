d2b9de3617617ac81e17289e33b11bc821cc53a6
Iqlaas <kamaliqlaas@gmail.com>
2018-08-29 12:28:04 +0800

    Update

---
+++ maps/dictionary.go
@@ -0,0 +1,38 @@
+package main
+
+func main() {
+}
+
+type Dictionary map[string]string
+
+const (
+	ErrNotFound   = DictionaryErr("could not find the word you are looking for")
+	ErrWordExists = DictionaryErr("word exists")
+)
+
+type DictionaryErr string
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
+
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+	if !ok {
+		return "", ErrNotFound
+	}
+	return definition, nil
+}
+
+func (d Dictionary) Add(word, definition string) error {
+	_, err := d.Search(word)
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+	return nil
+}
